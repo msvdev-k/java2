05c674dbd2a7d189fd99e874fd54064f4476ab7a
msvdev-k <msvdev.k@gmail.com>
2022-02-05 13:16:58 +0300

    Vitaly code

---
+++ src/main/java/com/geekbrains/OwnLinkedList.java
@@ -0,0 +1,69 @@
+package com.geekbrains;
+
+public class OwnLinkedList<T> {
+    private long size;
+    private Node head;
+    private Node tail;
+
+    public OwnLinkedList() {
+        this.size = 0;
+        this.head = null;
+        this.tail = null;
+    }
+
+    public void addNode(T data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+        }
+
+        tail = newNode;
+        size++;
+    }
+
+    public void display() {
+        Node current = head;
+
+        if (head == null) {
+            System.out.println("Односвязный список пуст");
+            return;
+        }
+
+        while (current != null) {
+            System.out.println(current.data + " ");
+            current = current.next;
+        }
+
+        System.out.println();
+    }
+
+    public void display(int index) {
+        if (index > size) {
+            System.out.println("Размер списка больше чем заданный индекс");
+            return;
+        }
+
+        Node current = head;
+
+        for (int i = 1; i <= index; i++) {
+            current = current.next;
+        }
+
+        System.out.println(current.data);
+    }
+
+
+    private class Node {
+        T data;
+        Node next;
+
+        public Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+}
